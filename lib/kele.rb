017515061efbe7305ffa8ebbfa6d52f6181a5893
robotscissors <cjbazin@gmail.com>
2017-06-14 21:36:38 -0700

    dry up code add private method

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,22 +8,30 @@ class Kele
 
   def initialize(email,password)
     @baseURL = 'https://www.bloc.io/api/v1/'
-    @email = email
-    @password = password
-    @response = self.class.post("#{@baseURL}sessions", body: {"email": @email, "password": @password})
-    raise "Invalid Credentials" if @response.code != 200
-    @auth_token = @response['auth_token']
+    response = self.class.post("#{@baseURL}sessions", body: {"email": email, "password": password})
+    raise "Invalid Credentials" if response.code != 200
+    @auth_token = response['auth_token']
     puts "Credentials Approved"
   end
 
   def get_me
-    @me = self.class.get("#{@baseURL}users/me", headers: { "authorization" => @auth_token })
-    JSON.parse(@me.body)
+    response = self.class.get("#{@baseURL}users/me", headers: auth_headers)
+    JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    @mentor_id = mentor_id
-    @available = self.class.get("#{@baseURL}mentors/#{@mentor_id}/student_availability", headers: { "authorization" => @auth_token } )
-    JSON.parse(@available.body).to_a
+    response = self.class.get("#{@baseURL}mentors/#{mentor_id}/student_availability", headers: auth_headers)
+    JSON.parse(response.body).to_a
+  end
+
+  def get_roadmap(roadmap_id)
+    response = self.class.get("#{@baseURL}roadmaps/#{roadmap_id}", headers: auth_headers)
+    JSON.parse(response.body).to_a
+  end
+
+  private
+
+  def auth_headers
+    { "authorization" => @auth_token }
   end
 end
