e552df4507f8aeb0bde90187ac60b536f18db2b6
robotscissors <cjbazin@gmail.com>
2017-06-18 17:44:11 -0700

    add parameter for submission ID that is returned when something has been created

--- lib/checkpoint.rb
+++ lib/checkpoint.rb
@@ -4,7 +4,7 @@ module Checkpoint
     response = self.class.post("#{@baseURL}checkpoint_submissions", body: {"assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": @enrollment_id}, headers: auth_headers)
   end
 
-  def update_checkpoint(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-    response = self.class.put("#{@baseURL}checkpoint_submissions/#{checkpoint_id}", body: {"assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": @enrollment_id}, headers: auth_headers)
+  def update_checkpoint(submission_id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.put("#{@baseURL}checkpoint_submissions/#{submission_id}", body: {"assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": @enrollment_id}, headers: auth_headers)
   end
 end
